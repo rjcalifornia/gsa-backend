d789deed2a1b70d9102f1a114a5156873b3c3080
rjcalifornia <rodolfohernan20@gmail.com>
2026-04-02 07:54:27 -0600

    Changes to be committed: 	new file:   app/Http/Controllers/main/DashboardController.php 	new file:   resources/views/app/dashboard.blade.php 	modified:   resources/views/layouts/app.blade.php 	modified:   routes/web.php

---
+++ resources/views/app/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title')
+Inicio
+@endsection
+
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\main\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -17,7 +18,5 @@ Route::get('/logout',[AuthController::class, 'logout'])->name('logout');
 
 
 Route::prefix('/')->group(function () {
-    Route::get('dashboard', function () {
-        return view('welcome');
-    })->name('homepage')->middleware('auth');
+    Route::get('dashboard', [DashboardController::class, 'main'])->name('homepage')->middleware('auth');
 });
